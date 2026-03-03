c63b99bb05909bf6f625e7e5b923671c0692181a
sugonhwang <sugonhwang1@gmail.com>
2026-03-03 13:09:27 +0900

    test: 구글 로그인 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+import { supabase } from "@/lib/supabaseClient";
+
 export default function Home() {
-  return <div>랜딩페이지</div>;
+  const handleGoogleLogin = async () => {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+    });
+
+    if (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <div>랜딩페이지</div>
+      <h1>KEYTAP</h1>
+      <button onClick={handleGoogleLogin}>Google로 로그인</button>
+    </div>
+  );
 }
