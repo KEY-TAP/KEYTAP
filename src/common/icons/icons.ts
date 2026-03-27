f30e108d92d0818390b73f2a52444403db2841a6
Yunning <ayh95944674@gmail.com>
2026-03-27 14:33:46 +0900

    feat : svg 아이콘 추가 및 컴포넌트화

---
+++ src/common/icons/icons.ts
@@ -0,0 +1,15 @@
+export const ICONS = {
+  github: "/github_icon.svg",
+  hearing: "/hearingIcon.svg",
+  insta: "/insta_icon.svg",
+  notion: "/notion_icon.svg",
+  play: "/playIcon.svg",
+  pause: "/pauseIcon.svg",
+} as const;
+
+export const GITHUB_ICON = ICONS.github;
+export const HEARING_ICON = ICONS.hearing;
+export const INSTA_ICON = ICONS.insta;
+export const NOTION_ICON = ICONS.notion;
+export const PLAY_ICON = ICONS.play;
+export const PAUSE_ICON = ICONS.pause;
