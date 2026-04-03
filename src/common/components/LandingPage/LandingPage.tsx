b67c159517fac399053f2372f5b15a48d8414daf
Yunning <ayh95944674@gmail.com>
2026-04-03 20:27:55 +0900

    feat : 랜딩 페이지

--- src/common/components/LandingPage/LandingPage.tsx
+++ src/common/components/LandingPage/LandingPage.tsx
@@ -1,9 +1,367 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
+import Image from "next/image";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+// 이미지
+import headphone01 from "../../../../public/headphone01.png";
+import keyboard01h from "../../../../public/keyboard01-2.png";
+import mouse01 from "../../../../public/mouse01.png";
+import mobilePhone01 from "../../../../public/mobilephone01.png";
+import keyboard02h from "../../../../public/keyboard02-2.png";
+import headset01 from "../../../../public/headset01.png";
+
+// mui
+import { styled } from "@mui/material/styles";
+
+// gsap 플러그인 등록
+gsap.registerPlugin(useGSAP);
 
 const LandingPage = () => {
-  return <div>랜딩2222222222</div>;
+  const rootRef = useRef<HTMLDivElement | null>(null);
+  const imageWrapRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(
+    () => {
+      const wrap = imageWrapRef.current;
+      if (!wrap) return;
+
+      // 무한 플로우
+      const marqueeTween = gsap.to(wrap, {
+        xPercent: -50,
+        duration: 20,
+        ease: "none",
+        repeat: -1,
+      });
+
+      const hoverItems = gsap.utils.toArray<HTMLElement>(".hover");
+      const cleanups: Array<() => void> = [];
+
+      hoverItems.forEach((item) => {
+        const text = item.querySelector<HTMLElement>("p");
+        if (!text) return;
+
+        gsap.set(text, {
+          x: 0,
+          y: 0,
+          autoAlpha: 0,
+          scale: 0.96,
+          pointerEvents: "none",
+        });
+
+        const getLocalPosition = (clientX: number, clientY: number) => {
+          const rect = item.getBoundingClientRect();
+
+          const tooltipWidth = text.offsetWidth;
+          const tooltipHeight = text.offsetHeight;
+
+          // 커서 기준 기본 위치
+          let x = clientX - rect.left;
+          let y = clientY - rect.top;
+
+          // hover 박스 안에서만 보이도록 범위 제한
+          const minX = tooltipWidth / 2;
+          const maxX = rect.width - tooltipWidth / 2;
+
+          const minY = tooltipHeight + 12;
+          const maxY = rect.height - 8;
+
+          x = gsap.utils.clamp(minX, maxX, x);
+          y = gsap.utils.clamp(minY, maxY, y);
+
+          return { x, y };
+        };
+
+        const moveText = (clientX: number, clientY: number) => {
+          const { x, y } = getLocalPosition(clientX, clientY);
+
+          gsap.set(text, {
+            x,
+            y,
+          });
+        };
+
+        const showTextAt = (clientX: number, clientY: number) => {
+          const { x, y } = getLocalPosition(clientX, clientY);
+
+          gsap.killTweensOf(text);
+
+          gsap.set(text, {
+            x,
+            y,
+            autoAlpha: 1,
+            scale: 1,
+          });
+        };
+
+        const hideText = () => {
+          gsap.killTweensOf(text);
+
+          gsap.to(text, {
+            autoAlpha: 0,
+            scale: 0.96,
+            duration: 0.16,
+            ease: "power2.out",
+            overwrite: true,
+          });
+        };
+
+        // PC
+        const handleMouseEnter = (e: MouseEvent) => {
+          marqueeTween.pause();
+          showTextAt(e.clientX, e.clientY);
+        };
+
+        const handleMouseMove = (e: MouseEvent) => {
+          moveText(e.clientX, e.clientY);
+        };
+
+        const handleMouseLeave = () => {
+          hideText();
+          marqueeTween.resume();
+        };
+
+        // Mobile
+        const handleTouchStart = (e: TouchEvent) => {
+          const touch = e.touches[0];
+          if (!touch) return;
+
+          marqueeTween.pause();
+          showTextAt(touch.clientX, touch.clientY);
+        };
+
+        const handleTouchMove = (e: TouchEvent) => {
+          const touch = e.touches[0];
+          if (!touch) return;
+
+          moveText(touch.clientX, touch.clientY);
+        };
+
+        const handleTouchEnd = () => {
+          hideText();
+          marqueeTween.resume();
+        };
+
+        item.addEventListener("mouseenter", handleMouseEnter);
+        item.addEventListener("mousemove", handleMouseMove);
+        item.addEventListener("mouseleave", handleMouseLeave);
+
+        item.addEventListener("touchstart", handleTouchStart, { passive: true });
+        item.addEventListener("touchmove", handleTouchMove, { passive: true });
+        item.addEventListener("touchend", handleTouchEnd);
+        item.addEventListener("touchcancel", handleTouchEnd);
+
+        cleanups.push(() => {
+          item.removeEventListener("mouseenter", handleMouseEnter);
+          item.removeEventListener("mousemove", handleMouseMove);
+          item.removeEventListener("mouseleave", handleMouseLeave);
+
+          item.removeEventListener("touchstart", handleTouchStart);
+          item.removeEventListener("touchmove", handleTouchMove);
+          item.removeEventListener("touchend", handleTouchEnd);
+          item.removeEventListener("touchcancel", handleTouchEnd);
+        });
+      });
+
+      return () => {
+        marqueeTween.kill();
+        cleanups.forEach((cleanup) => cleanup());
+      };
+    },
+    { scope: rootRef },
+  );
+
+  return (
+    <LandingWrapper ref={rootRef}>
+      <FlowArea>
+        <ImageWrap ref={imageWrapRef}>
+          <FlowRow>
+            <div className="hover">
+              <Image src={keyboard01h} alt="키보드이미지" className="bubble" />
+              <p>NUPHY KICK75</p>
+              <Image src={headphone01} alt="헤드폰이미지" />
+            </div>
+
+            <div>
+              <Image src={mouse01} alt="마우스 이미지" />
+              <Image src={mobilePhone01} alt="핸드폰이미지" />
+            </div>
+
+            <div className="hover">
+              <Image src={headset01} alt="이어폰이미지" />
+              <Image src={keyboard02h} alt="키보드이미지" className="bubble" />
+              <p>NUPHY KICK75</p>
+            </div>
+          </FlowRow>
+
+          <FlowRow aria-hidden="true">
+            <div className="hover">
+              <Image src={keyboard01h} alt="키보드이미지" className="bubble" />
+              <p>NUPHY KICK75</p>
+              <Image src={headphone01} alt="헤드폰이미지" />
+            </div>
+
+            <div>
+              <Image src={mouse01} alt="마우스 이미지" />
+              <Image src={mobilePhone01} alt="핸드폰이미지" />
+            </div>
+
+            <div className="hover">
+              <Image src={headset01} alt="이어폰이미지" />
+              <Image src={keyboard02h} alt="키보드이미지" className="bubble" />
+              <p>NUPHY KICK75</p>
+            </div>
+          </FlowRow>
+        </ImageWrap>
+      </FlowArea>
+
+      <ExamText>이미지를 클릭해 타건음을 체험해보세요.</ExamText>
+    </LandingWrapper>
+  );
 };
 
 export default LandingPage;
+
+// 스타일드 컴포넌트
+const LandingWrapper = styled("div")(() => ({
+  width: "100%",
+  height: "100vh",
+  backgroundImage: "url(/background.png)",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  overflow: "hidden",
+  position: "relative",
+}));
+
+const FlowArea = styled("div")(() => ({
+  overflow: "hidden",
+  width: "100%",
+  height: "100vh",
+}));
+
+const ImageWrap = styled("div")(() => ({
+  position: "relative",
+  display: "flex",
+  width: "max-content",
+  height: "100%",
+  willChange: "transform",
+}));
+
+const ExamText = styled("p")(({ theme }) => ({
+  position: "absolute",
+  bottom: "20px",
+  left: "50%",
+  transform: "translateX(-50%)",
+  zIndex: 9999,
+  fontSize: "1.25rem",
+  color: theme.palette.background.paper,
+}));
+
+const FlowRow = styled("div")(({ theme }) => ({
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  flexShrink: 0,
+
+  "& > div": {
+    display: "flex",
+    flexDirection: "column",
+
+    "&:first-of-type": {
+      "& img:first-of-type": {
+        [theme.breakpoints.down("md")]: {
+          width: "670px",
+        },
+      },
+      "& img:last-of-type": {
+        marginTop: "-280px",
+        aspectRatio: "1/1",
+
+        [theme.breakpoints.down("md")]: {
+          width: "360px",
+          marginTop: "-180px",
+        },
+      },
+    },
+
+    "&:nth-of-type(2)": {
+      justifyContent: "space-between",
+      margin: "40px 0",
+    },
+
+    "&:last-of-type": {
+      "& img:first-of-type": {
+        marginLeft: "auto",
+        marginRight: "100px",
+        marginTop: "170px",
+
+        [theme.breakpoints.down("md")]: {
+          width: "140px",
+        },
+      },
+      "& img:last-of-type": {
+        marginTop: "-140px",
+
+        [theme.breakpoints.down("md")]: {
+          width: "610px",
+          marginTop: "-90px",
+        },
+      },
+    },
+
+    "& img": {
+      height: "auto",
+      objectFit: "cover",
+    },
+  },
+
+  "& .hover": {
+    position: "relative",
+  },
+
+  "& .hover p": {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    zIndex: 10,
+    opacity: 1,
+
+    fontSize: "1.25rem",
+    fontWeight: "700",
+    color: theme.palette.primary.main,
+    backgroundColor: theme.palette.background.paper,
+    padding: "14px 40px",
+    borderRadius: "24px 24px 24px 0",
+    boxSizing: "border-box",
+    whiteSpace: "nowrap",
+    pointerEvents: "none",
+    willChange: "transform, opacity",
+    transform: "translate(-50%, calc(-100% - 12px))",
+
+    [theme.breakpoints.down("md")]: {
+      fontSize: "1.1rem",
+      padding: "12px 30px",
+      borderRadius: "22px 22px 22px 0",
+    },
+
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "1.1rem",
+      padding: "10px 26px",
+      borderRadius: "20px 20px 20px 0",
+    },
+  },
+
+  "& .bubble": {
+    transition: "opacity .3s ease",
+    opacity: 0.4,
+    position: "relative",
+  },
+
+  "& .hover:hover .bubble": {
+    opacity: 1,
+    cursor: "pointer",
+  },
+}));
