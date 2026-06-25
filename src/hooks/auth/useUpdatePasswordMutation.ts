c8cd75a12bbccf16c73876ca6aa33dcde418692b
Yunning <ayh95944674@gmail.com>
2026-06-25 23:36:12 +0900

    feat : 유저 비밀번호를 업데이트(변경)하는 Mutation Hook

---
+++ src/hooks/auth/useUpdatePasswordMutation.ts
@@ -0,0 +1,12 @@
+// 유저 비밀번호를 업데이트(변경)하는 Mutation Hook
+
+"use client";
+
+import { useMutation } from "@tanstack/react-query";
+import { updatePassword } from "@/api/auth/updatePassword";
+
+export function useUpdatePasswordMutation() {
+  return useMutation({
+    mutationFn: updatePassword,
+  });
+}
